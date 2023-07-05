348f5cab4e61fb1b760909cdc1b5f8d4fe6b50d2
valeriovinciarelli <Roma?=10>
2023-07-05 14:55:09 +0200

    fixes issues with environment variables

--- cmd/create.go
+++ cmd/create.go
@@ -1,232 +1,230 @@
 /*
-    EPOS Open Source - Local installation with Kubernetes
-    Copyright (C) 2023  EPOS ERIC
+   EPOS Open Source - Local installation with Kubernetes
+   Copyright (C) 2023  EPOS ERIC
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 //file: ./cmd/create.go
 package cmd
 
 import (
-    "github.com/spf13/cobra"
-	"github.com/joho/godotenv"
-    "os/exec"
-    "os"
-    "log"
-    "time"
-    "regexp"
-    "strings"
 	"github.com/a8m/envsubst"
+	"github.com/joho/godotenv"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
 )
 
 var (
 	lineBreakRegExp = regexp.MustCompile(`\r?\n`)
 )
 
-var deployCmd = & cobra.Command {
-    Use: "deploy",
-    Short: "Deploy an environment on Kubernetes",
-    Long: `Deploy an enviroment with .env set up on Kubernetes`,
-    Run: func(cmd * cobra.Command, args[] string) {
+var deployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "Deploy an environment on Kubernetes",
+	Long:  `Deploy an enviroment with .env set up on Kubernetes`,
+	Run: func(cmd *cobra.Command, args []string) {
 
-        env, _ := cmd.Flags().GetString("env")
-        context, _ := cmd.Flags().GetString("context")
-        namespace, _ := cmd.Flags().GetString("namespace")
-        tag, _ := cmd.Flags().GetString("tag")
+		env, _ := cmd.Flags().GetString("env")
+		context, _ := cmd.Flags().GetString("context")
+		namespace, _ := cmd.Flags().GetString("namespace")
+		tag, _ := cmd.Flags().GetString("tag")
 
-        if env == "" {
-            env = generateTempFile(configurations)
-        }
+		if env == "" {
+			env = generateTempFile(configurations)
+		}
 
-        fileContents, err := os.ReadFile(env)
-        if err != nil {
-            log.Fatalln(err)
-        }
+		fileContents, err := os.ReadFile(env)
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-        fileLines := lineBreakRegExp.Split(string(fileContents), -1)
+		fileLines := lineBreakRegExp.Split(string(fileContents), -1)
 
-        for _, line := range fileLines {
-            if strings.Contains(line, "="){
-                res1 := strings.Split(line, "=")
-                os.Setenv(res1[0], res1[1])
-            }
-        }
+		for _, line := range fileLines {
+			if strings.Contains(line, "=") {
+				res1 := strings.Split(line, "=")
+				os.Setenv(res1[0], res1[1])
+			}
+		}
 
-        os.Setenv("CONTEXT", context)
-        os.Setenv("DEPLOY_TAG", tag)
-        os.Setenv("NAMESPACE", namespace)
-        os.Setenv("DEPLOY_PATH", "/"+namespace+"/")
-        os.Setenv("BASE_CONTEXT", "/"+namespace)
+		os.Setenv("CONTEXT", context)
+		os.Setenv("DEPLOY_TAG", tag)
+		os.Setenv("NAMESPACE", namespace)
+		os.Setenv("DEPLOY_PATH", "/"+namespace+"/")
+		os.Setenv("BASE_CONTEXT", "/"+namespace)
+		os.Setenv("POSTGRESQL_CONNECTION_STRING", "jdbc:postgresql://"+os.Getenv("POSTGRESQL_HOST")+"/"+os.Getenv("POSTGRES_DB")+"?user="+os.Getenv("POSTGRES_USER")+"&password="+os.Getenv("POSTGRESQL_PASSWORD")+"")
 
-        setupIPs()
+		setupIPs()
 
-        operator, err = envsubst.Bytes([]byte(operator))
-        if err != nil {
-            log.Fatal(err)
-        }
-        rabbitmqoperatorfile := generateTempFile(operator)
+		operator, err = envsubst.Bytes([]byte(operator))
+		if err != nil {
+			log.Fatal(err)
+		}
+		rabbitmqoperatorfile := generateTempFile(operator)
 
-        rabbitmq, err = envsubst.Bytes([]byte(rabbitmq))
-        if err != nil {
-            log.Fatal(err)
-        }
-        rabbitmqfile := generateTempFile(rabbitmq)
+		rabbitmq, err = envsubst.Bytes([]byte(rabbitmq))
+		if err != nil {
+			log.Fatal(err)
+		}
+		rabbitmqfile := generateTempFile(rabbitmq)
 
-        logging, err = envsubst.Bytes([]byte(logging))
-        if err != nil {
-            log.Fatal(err)
-        }
-        loggingfile := generateTempFile(logging)
+		logging, err = envsubst.Bytes([]byte(logging))
+		if err != nil {
+			log.Fatal(err)
+		}
+		loggingfile := generateTempFile(logging)
 
-        secrets, err = envsubst.Bytes([]byte(secrets))
-        if err != nil {
-            log.Fatal(err)
-        }
-        secretsfile := generateTempFile(secrets)
+		secrets, err = envsubst.Bytes([]byte(secrets))
+		if err != nil {
+			log.Fatal(err)
+		}
+		secretsfile := generateTempFile(secrets)
 
-        backoffice, err = envsubst.Bytes([]byte(backoffice))
-        if err != nil {
-            log.Fatal(err)
-        }
-        backofficefile := generateTempFile(backoffice)
+		backoffice, err = envsubst.Bytes([]byte(backoffice))
+		if err != nil {
+			log.Fatal(err)
+		}
+		backofficefile := generateTempFile(backoffice)
 
-        dataMetadata, err = envsubst.Bytes([]byte(dataMetadata))
-        if err != nil {
-            log.Fatal(err)
-        }
-        datametadatafile := generateTempFile(dataMetadata)
+		dataMetadata, err = envsubst.Bytes([]byte(dataMetadata))
+		if err != nil {
+			log.Fatal(err)
+		}
+		datametadatafile := generateTempFile(dataMetadata)
 
-        externalAccess, err = envsubst.Bytes([]byte(externalAccess))
-        if err != nil {
-            log.Fatal(err)
-        }
-        externalaccessfile := generateTempFile(externalAccess)
+		externalAccess, err = envsubst.Bytes([]byte(externalAccess))
+		if err != nil {
+			log.Fatal(err)
+		}
+		externalaccessfile := generateTempFile(externalAccess)
 
-        ingestor, err = envsubst.Bytes([]byte(ingestor))
-        if err != nil {
-            log.Fatal(err)
-        }
-        ingestorfile := generateTempFile(ingestor)
+		ingestor, err = envsubst.Bytes([]byte(ingestor))
+		if err != nil {
+			log.Fatal(err)
+		}
+		ingestorfile := generateTempFile(ingestor)
 
-        metadataDatabase, err = envsubst.Bytes([]byte(metadataDatabase))
-        if err != nil {
-            log.Fatal(err)
-        }
-        metadatadatabasefile := generateTempFile(metadataDatabase)
+		metadataDatabase, err = envsubst.Bytes([]byte(metadataDatabase))
+		if err != nil {
+			log.Fatal(err)
+		}
+		metadatadatabasefile := generateTempFile(metadataDatabase)
 
-        redisDatabase, err = envsubst.Bytes([]byte(redisDatabase))
-        if err != nil {
-            log.Fatal(err)
-        }
-        redisdatabasefile := generateTempFile(redisDatabase)
+		redisDatabase, err = envsubst.Bytes([]byte(redisDatabase))
+		if err != nil {
+			log.Fatal(err)
+		}
+		redisdatabasefile := generateTempFile(redisDatabase)
 
-        resources, err = envsubst.Bytes([]byte(resources))
-        if err != nil {
-            log.Fatal(err)
-        }
-        resourcesfile := generateTempFile(resources)
+		resources, err = envsubst.Bytes([]byte(resources))
+		if err != nil {
+			log.Fatal(err)
+		}
+		resourcesfile := generateTempFile(resources)
 
-        gateway, err = envsubst.Bytes([]byte(gateway))
-        if err != nil {
-            log.Fatal(err)
-        }
-        gatewayfile := generateTempFile(gateway)
+		gateway, err = envsubst.Bytes([]byte(gateway))
+		if err != nil {
+			log.Fatal(err)
+		}
+		gatewayfile := generateTempFile(gateway)
 
-        converter, err = envsubst.Bytes([]byte(converter))
-        if err != nil {
-            log.Fatal(err)
-        }
-        converterfile := generateTempFile(converter)
+		converter, err = envsubst.Bytes([]byte(converter))
+		if err != nil {
+			log.Fatal(err)
+		}
+		converterfile := generateTempFile(converter)
 
-        list_of_services:= [13]string{rabbitmqoperatorfile, rabbitmqfile, loggingfile, secretsfile, 
-            backofficefile, datametadatafile, externalaccessfile, ingestorfile, metadatadatabasefile, 
-            redisdatabasefile, resourcesfile, gatewayfile, converterfile}
+		list_of_services := [13]string{rabbitmqoperatorfile, rabbitmqfile, loggingfile, secretsfile, metadatadatabasefile,
+			backofficefile, datametadatafile, externalaccessfile, ingestorfile,
+			redisdatabasefile, resourcesfile, gatewayfile, converterfile}
 
-        if err := godotenv.Load(env);
-        err != nil {
-            log.Fatal("Error loading env variables from "+env+"\n")
-            log.Fatal(err)
-        }
+		if err := godotenv.Load(env); err != nil {
+			log.Fatal("Error loading env variables from " + env + "\n")
+			log.Fatal(err)
+		}
 
-        cmd.Printf(">> Deploy environment\n   >> Context: %s \n   >> Namespace: %s \n   >> Env file: %s \n   >> LocalAddress ip %s\n", context, namespace, env, os.Getenv("LOCAL_IP"))
+		cmd.Printf(">> Deploy environment\n   >> Context: %s \n   >> Namespace: %s \n   >> Env file: %s \n   >> LocalAddress ip %s\n", context, namespace, env, os.Getenv("LOCAL_IP"))
 
-        execute_command(cmd,exec.Command("kubectl",
-        "config",
-        "use-context",
-        context))
+		execute_command(cmd, exec.Command("kubectl",
+			"config",
+			"use-context",
+			context))
 
-        execute_command(cmd,exec.Command("kubectl",
-        "apply",
-        "-f",
-        "https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.1/deploy/static/provider/cloud/deploy.yaml"))
+		execute_command(cmd, exec.Command("kubectl",
+			"apply",
+			"-f",
+			"https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.1/deploy/static/provider/cloud/deploy.yaml"))
 
-        execute_command(cmd,exec.Command("kubectl",
-        "create",
-        "ns",
-        namespace))
+		execute_command(cmd, exec.Command("kubectl",
+			"create",
+			"ns",
+			namespace))
 
-        time.Sleep(10 * time.Second)
+		time.Sleep(10 * time.Second)
 
-        execute_command(cmd,exec.Command("kubectl",
-            "apply",
-            "-f",
-            list_of_services[0]))
+		execute_command(cmd, exec.Command("kubectl",
+			"apply",
+			"-f",
+			list_of_services[0]))
 
-        time.Sleep(10 * time.Second)
+		time.Sleep(10 * time.Second)
 
-        for i:= 1; i < 13; i++{
+		for i := 1; i < 13; i++ {
 
-            execute_command(cmd,exec.Command(
-            "kubectl",
-            "apply",
-            "-f",
-            list_of_services[i],
-            "-n",
-            namespace))
-            
+			execute_command(cmd, exec.Command(
+				"kubectl",
+				"apply",
+				"-f",
+				list_of_services[i],
+				"-n",
+				namespace))
 
-            execute_command(cmd,exec.Command("kubectl",
-            "wait",
-            "--for=condition=Ready",
-            "pods",
-            "--all",
-            "-n",
-            namespace))
+			execute_command(cmd, exec.Command("kubectl",
+				"wait",
+				"--for=condition=Ready",
+				"pods",
+				"--all",
+				"-n",
+				namespace))
 
-            time.Sleep(10 * time.Second)
-        }
+			time.Sleep(10 * time.Second)
+		}
 
-        print_urls()
+		print_urls()
 
-    },
+	},
 }
 
-func execute_command(cmd * cobra.Command, command * exec.Cmd) {
-    cmd.Println(command.String())
-    command.Stdout = os.Stdout
-    command.Stderr = os.Stderr
-    if err := command.Run();
-    err != nil {
-        print(err)
-    }
+func execute_command(cmd *cobra.Command, command *exec.Cmd) {
+	cmd.Println(command.String())
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	if err := command.Run(); err != nil {
+		print(err)
+	}
 }
 
 func init() {
-    deployCmd.Flags().String("context", "", "Kubernetes context")
-    deployCmd.MarkFlagRequired("context")
-    deployCmd.Flags().String("namespace", "", "Kubernetes namespace")
-    deployCmd.MarkFlagRequired("namespace")
-    deployCmd.Flags().String("tag", "", "Version Tag")
-    deployCmd.MarkFlagRequired("tag")
+	deployCmd.Flags().String("context", "", "Kubernetes context")
+	deployCmd.MarkFlagRequired("context")
+	deployCmd.Flags().String("namespace", "", "Kubernetes namespace")
+	deployCmd.MarkFlagRequired("namespace")
+	deployCmd.Flags().String("tag", "", "Version Tag")
+	deployCmd.MarkFlagRequired("tag")
 }
